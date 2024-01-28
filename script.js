4a5c2aaca0a8b151ae104edd2549e1d241d5c20c
Anurag Jaiswal <janurag582004@gmail.com>
2024-01-28 13:41:05 +0530

    .

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 
-
 let hrs = document.getElementById("hrs");
 let min = document.getElementById("min");
 let sec = document.getElementById("sec");
-
 
 setInterval(() => {
     let currentTime = new Date();
@@ -11,4 +9,5 @@ setInterval(() => {
     hrs.innerHTML = (currentTime.getHours()<10?"0":"") + currentTime.getHours();
     min.innerHTML = (currentTime.getMinutes()<10?"0":"") + currentTime.getMinutes();
     sec.innerHTML = (currentTime.getSeconds()<10?"0":"") + currentTime.getSeconds();
+    
 }, 1000)
